dddc3e02e7dfc0a1543d71ed4d40e60dc37281e8
cloudwraith <cloudwraith1@gmail.com>
2018-04-04 14:23:03 +0500

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
-  def new
-  end
+
+
+  	def new
+  	end
 
 	def create
 	  	user = User.find_by(email: params[:session][:email].downcase)
 	    if user && user.authenticate(params[:session][:password])
 	      # Log the user in and redirect to the user's show page.
 	      log_in user
+	      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 	      redirect_to user
 	    else
 	      # Create an error message.
@@ -16,7 +19,20 @@ class SessionsController < ApplicationController
   	end
 
 	def destroy
-  		log_out
+  		log_out if logged_in?
   		redirect_to root_url
+  	end
+
+  	# Returns the user corresponding to the remember token cookie.
+  	def current_user
+    	if (user_id = session[:user_id])
+      		@current_user ||= User.find_by(id: user_id)
+    	elsif (user_id = cookies.signed[:user_id])
+      		user = User.find_by(id: user_id)
+      		if user && user.authenticated?(cookies[:remember_token])
+        		log_in user
+        		@current_user = user
+      		end
+    	end
   	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325141853) do
+ActiveRecord::Schema.define(version: 20180403133424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180325141853) do
     t.datetime "updated_at", null: false
     t.string "age"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
